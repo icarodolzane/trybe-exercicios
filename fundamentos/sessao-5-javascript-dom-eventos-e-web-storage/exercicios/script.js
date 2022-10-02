b0a92732f5185d7ecd56b22fd5d24d93c914c168
Icaro Dolzane <icarodolzane@live.com>
2022-10-02 19:15:12 -0400

    Finalizado os exercícios da 5.2

--- fundamentos/sessao-5-javascript-dom-eventos-e-web-storage/exercicios/script.js
+++ fundamentos/sessao-5-javascript-dom-eventos-e-web-storage/exercicios/script.js
@@ -1,68 +1,129 @@
-/*
-     Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
-     - document.getElementById()
-     - document.getElementsByClassName()
-     - document.getElementsByTagName()
-     1. Crie e execute uma função que mude o texto na tag `<p>-----</p>`, para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício) OK
-     2. Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)). OK
-     3. Crie e execute uma função que mude a cor do quadrado vermelho para branco. OK
-     4. Crie e execute uma função que corrija o texto da tag <h1>. OK 
-     5. Crie e execute uma função que modifique o texto da primeira tag <p> para maiúsculo. OK
-     6. Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
-     */
+//globals
 
-const newParagraph = document.getElementById('meInTheFuture');
+const getBody = document.getElementsByTagName('body')[0];
+let itemList = document.createElement('li');
 
-changeText(newParagraph, 'Ganhando rios de dinheiro, estabilidade financeira e no emprego dos sonhos...');
-
-const mainContainer = document.getElementsByClassName('main-content');
-changeMainColor(mainContainer, 'rgb(76,164,109)');
-
-const centerContent = document.getElementsByClassName('center-content');
-changeCenterContent(centerContent, 'white');
-
-const title = document.getElementsByClassName('title')[0];
-changeText(title, 'Exercício - Javascript - D.O.M');
-
-const firstTagP = document.querySelector('p');
-
-firstTagP.style.marginLeft = '2%';
-
-changeText(firstTagP, 'Como você se vê daqui a 2 anos? ');
-toUpperCase(firstTagP);
-
-const trybeParagraph = document.querySelectorAll('p')[2].style.marginLeft = '1%';
-
-const tagPValues = document.querySelectorAll('p');
-
-printIntheConsole(tagPValues);
-
-function changeText(element, str) {
-    element.innerText = str;
-    return;
-}
-
-function changeMainColor(element, color){
-    element[0].style.backgroundColor = color;
-    return;
-}
-
-function changeCenterContent(element, color){
-    element[0].style.backgroundColor = color;
-    return;
-}
-
-function toUpperCase(element){
-    element.style.textTransform = 'uppercase';
-}
-
-function printIntheConsole (element){
+// functions
+function createTitle (){
     
-    for(let index = 0; index < element.length; index += 1){
-        console.log(element[index]);
-    }
+    const createH1 = document.createElement('h1');
+    createH1.innerText='Exercício - JavaScript DOM';
+    getBody.appendChild(createH1);
+    createH1.className = 'title';
+};
 
-    return; 
+function createMain (){
+    const createMain = document.createElement('main');
+    createMain.className = 'main-content';
+    createMain.style.border='1px solid black';
+    getBody.appendChild(createMain);
+    
+};
+
+function createSection(someClassName){
+    const createSection = document.createElement('section');
+    getBody.childNodes[3].appendChild(createSection);
+    createSection.className = someClassName;
+    createSection.style.border='1px solid black';
+};
+
+function createParagraph(string){
+    const createsParagraph = document.createElement('p');
+    createsParagraph.innerText = string;
+    document.getElementsByTagName('section')[0].appendChild(createsParagraph);
+    
+    return;
 }
+
+function createImage (link, someClassName){
+    const someImg = document.createElement('img');
+    someImg.src = link;
+    document.getElementsByClassName('left-content')[0].appendChild(someImg);
+    someImg.className = someClassName;
+
+    return;
+
+};
+
+function createUnorderedList(){
+    const someUl = document.createElement('ul');
+    document.getElementsByClassName('right-content')[0].appendChild(someUl);
+
+    let numbersExt = {
+        1: 'um',
+        2: 'dois',
+        3: 'três',
+        4: 'quatro',
+        5: 'cinco',
+        6: 'seis',
+        7: 'sete',
+        8: 'oito',
+        9: 'nove',
+        10: 'dez',
+      }; 
+        for (let number in numbersExt){
+            
+            let itemList = document.createElement('li');
+            itemList.innerText = numbersExt[number];
+            document.querySelector('.right-content').appendChild(itemList);
+        };        
+     
+
+}
+
+function createH3(string){
+    const someH3 = document.createElement('h3');
+    document.getElementsByTagName('main')[0].appendChild(someH3);
+    someH3.innerText = string;
+    someH3.className = 'description';
+    return;
+};
+
+function removeElement(){
+    const removeSectionLeft = document.getElementsByClassName('left-content')[0];
+    const getMain = document.getElementsByClassName('main-content')[0];
+    getMain.removeChild(removeSectionLeft);
+
+    return;
+};
+
+function getElementToCenter(){
+    const getSectionLeft = document.getElementsByClassName('right-content')[0];
+    getSectionLeft.style.marginRight = 'auto';
+
+    return;
+
+};
+
+function changeBackGroundColor(){
+    const getCenterSection = document.getElementsByClassName('center-content')[0];
+    getCenterSection.parentElement.style.backgroundColor = 'green';
+};
+
+function removeItemsList(){
+    const getList = document.getElementsByTagName('li');
+    getList[9].remove();
+    getList[8].remove();
+}
+
+//functions-calls
+createTitle ();
+createMain ();
+createSection('center-content');
+createParagraph("Is there a time for keeping your distance A time to turn your eyes away Is there a time for keeping your head down For getting on with your day");
+createSection('left-content');
+createSection('right-content');
+createImage('https://picsum.photos/200', 'small-image');
+createUnorderedList();
+createH3("Eu não aguento mais kkkk");
+createH3("Mas eu preciso muito de um bom emprego que me dê dinheiro kkkk");
+createH3("Eu não nasci herdeiro!!!");
+removeElement();
+getElementToCenter();
+changeBackGroundColor();
+removeItemsList();
+
+
+ 
 
 
